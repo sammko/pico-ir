646c0a46a7197e7121090edac99c89b1286ca7e4
Samuel Čavoj <samuel@cavoj.net>
2025-10-05 11:31:47 +0100

    Add more MQTT features

--- pico-ir-mqtt/src/main.rs
+++ pico-ir-mqtt/src/main.rs
@@ -1,5 +1,71 @@
+use ::std::str::{self, FromStr};
+
+use ::anyhow::{Context, anyhow, bail};
 use ::rumqttc as mq;
-use anyhow::{Context, bail};
+
+#[derive(Clone, Copy, Debug)]
+enum InfraredCommand {
+    TogglePower,
+    SetInput(AudioInput),
+    Raw(u8),
+}
+
+#[derive(Clone, Copy, Debug)]
+enum AudioInput {
+    Bluetooth,
+    _3_5mm,
+    Optical,
+    Rca,
+}
+
+impl FromStr for AudioInput {
+    type Err = ::anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "bluetooth" => Ok(Self::Bluetooth),
+            "3.5mm" => Ok(Self::_3_5mm),
+            "optical" => Ok(Self::Optical),
+            "rca" => Ok(Self::Rca),
+            _ => Err(anyhow!("invalid audio input string")),
+        }
+    }
+}
+
+impl InfraredCommand {
+    pub fn as_u8(&self) -> u8 {
+        match self {
+            InfraredCommand::TogglePower => 0x66,
+            InfraredCommand::SetInput(AudioInput::Bluetooth) => 0x86,
+            InfraredCommand::SetInput(AudioInput::_3_5mm) => 0x97,
+            InfraredCommand::SetInput(AudioInput::Optical) => 0x88,
+            InfraredCommand::SetInput(AudioInput::Rca) => 0x96,
+            InfraredCommand::Raw(b) => *b,
+        }
+    }
+
+    pub fn as_u32_le(&self) -> u32 {
+        const ADDRESS: u32 = 0x2385;
+        (self.as_u8() as u32) << 24 | (!self.as_u8() as u32) << 16 | ADDRESS
+    }
+}
+
+impl TryFrom<mq::Publish> for InfraredCommand {
+    type Error = ::anyhow::Error;
+
+    fn try_from(msg: mq::Publish) -> Result<Self, Self::Error> {
+        let Some(topic) = msg.topic.strip_prefix("jabu/pico-ir/") else {
+            bail!("topic prefix wrong");
+        };
+        let command = match topic {
+            "power" => InfraredCommand::TogglePower,
+            "input" => InfraredCommand::SetInput(str::from_utf8(&msg.payload)?.parse()?),
+            "raw" => InfraredCommand::Raw(u8::from_str_radix(str::from_utf8(&msg.payload)?, 16)?),
+            cmd => bail!("invalid command '{cmd}'"),
+        };
+        Ok(command)
+    }
+}
 
 fn main() -> ::anyhow::Result<()> {
     let mut serial = ::serialport::new("/dev/serial/by-id/usb-Jabu_Infrared_1-if00", 115200)
@@ -14,15 +80,17 @@ fn main() -> ::anyhow::Result<()> {
     client.subscribe("jabu/pico-ir/#", mq::QoS::AtMostOnce)?;
     for ev in conn.iter() {
         let ev = ev.context("got connection error")?;
-        match ev {
-            rumqttc::Event::Incoming(mq::Packet::Publish(mq::Publish { topic, .. }))
-                if topic == "jabu/pico-ir/power" =>
-            {
-                serial.write(b"66992385").context("serial write failed")?;
-                println!("sent");
+        let rumqttc::Event::Incoming(mq::Packet::Publish(msg)) = ev else {
+            continue;
+        };
+        let command = match InfraredCommand::try_from(msg) {
+            Ok(command) => command,
+            Err(e) => {
+                eprintln!("failed to parse message: {e}");
+                continue;
             }
-            _ => {}
-        }
+        };
+        write!(serial, "{:x}", command.as_u32_le()).context("failed to write to serial port")?;
     }
     bail!("wtf loop died");
 }
